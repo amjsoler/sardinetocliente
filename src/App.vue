29ae392b91e6463385c852d31af496fc3d0d7957
Jorge Amat Soler <amjsoler@gmail.com>
2024-01-24 00:21:38 +0100

    Implementando iniciar sesión

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <template>
   <router-view />
-
-  <ul class="mt-28 fixed bottom-0">
-    <li>
-      <router-link :to="{ name: 'LoginUser' }">Login de usuario</router-link>
-    </li>
-    <li>
-      <router-link :to="{ name: 'RegisterUser' }">Registro de usuario</router-link>
-    </li>
-  </ul>
 </template>
 
 <script>
+
 export default {
-  name: 'App'
+  name: 'App',
 }
 </script>
+
+    FooterMenu
---
+++ src/components/FooterMenu.vue
@@ -0,0 +1,16 @@
+<template>
+     <ul class="mt-28 fixed bottom-0 w-full flex flex-row text-white gap-4 justify-center h-12 bg-black items-center">
+    <li>
+      <router-link :to="{ name: 'LoginUser' }">Login de usuario</router-link>
+    </li>
+    <li>
+      <router-link :to="{ name: 'RegisterUser' }">Registro de usuario</router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+    name: "FooterMenu",
+}
+</script>
