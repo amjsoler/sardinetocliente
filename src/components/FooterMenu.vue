32310a61683fa73e10a406dad13d0ae7c2c2d0c2
Jorge Amat Soler <amjsoler@gmail.com>
2024-02-08 16:59:23 +0100

    Refactorizando footer

--- src/components/FooterMenu.vue
+++ src/components/FooterMenu.vue
@@ -2,13 +2,22 @@
      <footer class="fixed bottom-0 w-full h-12 bg-container-background flex flex-row
      justify-center [&>p]:flex-grow border-t-2 border-input-background">
       <p class="flex flex-col items-center justify-center">
-        <shopping-bag-icon @click="router().push({name: 'GymArticles'})"></shopping-bag-icon>
+        <shopping-bag-icon @click="router().push({name: 'GymArticles'})" />
+        <span class="text-[10px]">
+          {{ $t("footerMenu.shopBtn") }}
+        </span>
       </p>
       <p class="flex flex-col items-center justify-center">
-        <calendar-time-icon ></calendar-time-icon>
+        <calendar-time-icon />
+        <span class="text-[10px]">
+          {{ $t("footerMenu.classesBtn") }}
+        </span>
       </p>
       <p class="flex flex-col items-center justify-center">
-        <barbell-icon @click="router().push({name: 'UserExercises'})"></barbell-icon>
+        <barbell-icon @click="router().push({name: 'UserExercises'})" />
+        <span class="text-[10px]">
+          {{ $t("footerMenu.userExercises") }}
+        </span>
       </p>
      </footer>
 </template>
@@ -21,11 +30,13 @@ import BarbellIcon from '@/components/icons/BarbellIcon.vue'
 
 export default {
     name: "FooterMenu",
+
   methods: {
     router() {
       return router
     }
   },
+
   components: { BarbellIcon, CalendarTimeIcon, ShoppingBagIcon }
 }
 </script>
